2958c99ca439093531901cbda785a157faed96e0
Daniel <dando18studios@gmail.com>
2018-10-10 17:22:25 -0400

    added doxygen support

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -1,3 +1,9 @@
+/** @file Matrix.hpp
+	Contains Matrix class definition and implementation.
+	@author Daniel Nichols
+	@date October 2018
+*/
+
 #ifndef _MATRIX_H_
 #define _MATRIX_H_
 
@@ -5,62 +11,192 @@
 #include <stdexcept>	// std::invalid_argument
 #include "typedefs.h"	// uint
 
+
 namespace math {
 
 
+/** @brief Matrix generic class
+	
+	@author Daniel Nichols
+	@date October 2018
+*/
 template<typename N>
 class Matrix {
 	public:
+		/** Square matrix constructor. Creates a size*size matrix
+			with every value set to fill.
+			@param size - size of the rows and cols of the matrix
+			@param fill - default value for every entry
+		*/
 		Matrix(uint size, N fill);
+
+		/** Matrix constructor. Creates a rows*cols matrix
+			with every value set to fill.
+			@param rows - number of rows
+			@param cols - number of cols
+			@param fill - default value for every entry
+		*/
 		Matrix(uint rows, uint cols, N fill);
-		Matrix(const Matrix&);
 
-		N at(uint r, uint c);
+		/** Copy constructor. Copies matrix m into new matrix.
+			@param m - matrix to be copied
+		*/
+		Matrix(const Matrix& m);
+
+		/** Get element at r, c of the matrix 0-indexed.
+			@param r - row of return element
+			@param c - column of return element
+			@throw invalid_argument thrown if r<0 or r>=rows() or c<0 or c>=cols()
+		*/
+		N at(uint r, uint c) const;
+
+		/** Set element at r, c of the matrix 0-indexed.
+			@param r - row of element set
+			@param c - column of element set
+			@param val - value to set element at r,c
+			@throw invalid_argument thrown if r<0 or r>=rows() or c<0 or c>=cols()
+		*/
 		void set(uint r, uint c, N val);
+	
+		/** Get the shape or (rows, cols). This is equivalent to `std::make_pair(rows(), cols());`
+			@return an STL pair containing the row count and column count 
+		*/	
+		std::pair<uint, uint> shape() const { return std::make_pair(_rows, _cols); }
 
-		N* row(uint r);		// returns a copy
-		N* col(uint c);		// returns a copy
+		/** Get the number of rows in the matrix.
+			@return the number of rows in the matrix
+		*/
+		uint rows() const { return _rows; }
+		
+		/**	Get the number of columns in the matrix.
+			@return the number of columns in the matrix
+		*/
+		uint cols() const { return _cols; }
 
-		std::pair<uint, uint> shape() { return std::make_pair(_rows, _cols); }
-		uint rows() { return _rows; }
-		uint cols() { return _cols; }
-		uint size() { return _size; }
-
+		/** Get the size of the matrix (`size() == rows() * cols()`)
+			@return the size of the matrix
+		*/
+		uint size() const { return _size; }
 
 
 		// overloaded operators
-		Matrix& operator+=(const Matrix&);
-		Matrix& operator-=(const Matrix&);
-		Matrix& operator*=(const N&);
-		Matrix& operator*=(const Matrix&);
-		Matrix& operator/=(const Matrix&);
+
+		/** Copies `m` into `this`. Performs an element-wise copy.
+			Ignores self-copy.
+			@param m - matrix to copy into `this`
+			@return pointer to `this` after copy
+		*/
+		Matrix& operator=(const Matrix& m);
+
+		/**	Adds matrix `m` to `this` element-wise
+			@param m - matrix to add to `this`. rows and cols must be equivalent.
+			@return a pointer to `this` after addition
+			@throw invalid_argument thrown if `rows()!=m.rows()` or `cols()!=m.cols()`
+		*/
+		Matrix& operator+=(const Matrix& m);
+
+		/**	Subtracts matrix `m` from `this` element-wise
+			@param m - matrix to subtract from `this`. rows and cols must be equivalent.
+			@return a pointer to `this` after subtraction
+			@throw invalid_argument thrown if `rows()!=m.rows()` or `cols()!=m.cols()`
+		*/
+		Matrix& operator-=(const Matrix& m);
+
+		/**	Adds Multiplies `this` by scaler `scal`
+			@param scal - scaler to multiply `this` by
+			@return a pointer to `m` after multiplication
+		*/
+		Matrix& operator*=(const N& scal);
+
+		/**	Performs matrix multiplication between `this` and `m`. This operation will
+			throw an exception if `cols!=m.rows()`. It will also reshape `this` to that
+			`rows()` does not change and `cols()` becomes `m.cols()`.
+			@param m - matrix to multiply by `this`. 
+			@return a pointer to `this` after multiplication.
+			@throw invalid_argument if `cols()!=m.rows()` matrix multiplication is undefined
+		*/
+		Matrix& operator*=(const Matrix& m);
+
+		/**	Divides `this` by scalar `scal` element-wise.
+			Does not check for `scal==0` as division for class `N`
+			might have non-standard definition.
+			@param scal - scalar to divide `this` by
+			@return a pointer to `m` after division
+		*/
+		Matrix& operator/=(const N& scal);
 		
+		/** Destructor. Deletes the matrix internally */
 		~Matrix();
 		
 	private:
-		uint _size;
-		uint _cols;
-		uint _rows;
-		N** _matrix;
+		uint _size;		/**<size of matrix*/
+		uint _cols;		/**<number of columns in matrix*/
+		uint _rows;		/**<number of rows in matrix*/
+		N** _matrix;	/**<internal array to store matrix data*/
 };
 
 // free standing operator declarations
+/**	Adds lhs and rhs matrices element-wise. Copies lhs and add rhs to it.
+	@param lhs - left hand side matrix of addition
+	@param rhs - right hand side matrix of addition
+	@return a new matrix with elements from element-wise addition of `lhs` and `rhs`
+	@throw invalid_argument if `lhs` and `rhs` do not have same shape
+*/
 template<typename N>
-Matrix<N> operator+(Matrix<N> ,const Matrix<N>&);
+Matrix<N> operator+(Matrix<N> lhs,const Matrix<N>& rhs);
+
+/**	Subtracts lhs and rhs matrices element-wise. Copies lhs and subtract rhs from it.
+	@param lhs - left hand side matrix of subtraction
+	@param rhs - right hand side matrix of subtraction
+	@return a new matrix with elements from element-wise subtraction of `lhs` and `rhs`
+	@throw invalid_argument if `lhs` and `rhs` do not have same shape
+*/
 template<typename N>
-Matrix<N> operator-(Matrix<N>, const Matrix<N>&);
+Matrix<N> operator-(Matrix<N> lhs, const Matrix<N>& rhs);
+
+/**	Multiplies lhs and scalar rhs. Copies lhs and multiplies by scalar rhs
+	@param lhs - left hand side matrix 
+	@param rhs - right hand side scalar
+	@return a new matrix with elements multiplication of `lhs` and `rhs`
+*/
 template<typename N>
-Matrix<N> operator*(Matrix<N>, const N&);
+Matrix<N> operator*(Matrix<N> lhs, const N& rhs);
+
+/**	Multiplies scalar lhs and matrix rhs. Copies rhs and multiplies by scalar lhs
+	@param lhs - left hand side scalar 
+	@param rhs - right hand side matrix
+	@return a new matrix with elements multiplication of `lhs` and `rhs`
+*/
 template<typename N>
-Matrix<N> operator/(Matrix<N>, const N&);
+Matrix<N> operator*(const N& lhs, Matrix<N> rhs);
+
+/**	Divides lhs matrix by scalar rhs. Copies lhs and divides by scalar rhs.
+	Does not check if rhs is zero due to unknown type of `N`.
+	@param lhs - left hand side matrix 
+	@param rhs - right hand side scalar
+	@return a new matrix with elements division of `lhs` and `rhs`
+*/
 template<typename N>
-Matrix<N> operator*(Matrix<N>, const Matrix<N>&);
+Matrix<N> operator/(Matrix<N> lhs, const N& rhs);
+
+/**	Performs matrix multiplication of `rhs` and `lhs`
+	@param lhs - left hand side matrix
+	@param rhs - right hand side matrix
+	@return a new matrix resulting from matrix multiplication. Result will have shape `lhs.rows()`, `rhs.cols()`.
+	@throw invalid_argument if `lhs.cols() != rhs.rows()`
+*/
+template<typename N>
+Matrix<N> operator*(Matrix<N> lhs, const Matrix<N>& rhs);
 
 
 // define standard Matrix classes for easier use
+/** integer matrix */
 typedef Matrix<int> iMatrix;
+/** float precision matrix */
 typedef Matrix<float> fMatrix;
+/** double precision matrix */
 typedef Matrix<double> dMatrix;
+
 
 
 // implementation
@@ -129,7 +265,7 @@ Matrix<N>::Matrix(const Matrix& m) : _size(m.size()), _cols(m.cols()), _rows(m.r
 	throws invalid_argument if r or c are not within [0, rows()-1] or [0, cols()-1]
 */
 template<typename N>
-N Matrix<N>::at(uint r, uint c) {
+N Matrix<N>::at(uint r, uint c) const {
 	if (r < 0 || r >= _rows) 
 		throw std::invalid_argument("row out of range");
 	if (c < 0 || c >= _cols)
@@ -156,11 +292,45 @@ void Matrix<N>::set(uint r, uint c, N val) {
 }
 
 
+////////////////////
+// 			operator overload implementations
+////////////////////
+
+template<typename N>
+Matrix<N>& Matrix<N>::operator=(const Matrix& m) {
+	if (this != &m) {	// ignore self-assignment
+		if (_rows != m._rows || _cols != m._cols) {		// cannot reuse memory
+	
+			// delete current memory
+			for (int r = 0; r < _rows; ++r) delete[] _matrix[r];
+			delete[] _matrix;
+
+			// create new memory and reinit property vars
+			_size = 0;
+			_rows = 0;
+			_cols = 0;			
+
+			_matrix = new N*[m._rows];
+			for (int r = 0; r < m._rows; ++r) _matrix[r] = new N[m._cols];
+			
+			_size = m._size;
+			_rows = m._rows;
+			_cols = m._cols;	
+		} 
+		for (int r = 0; r < _rows; ++r)
+			for (int c = 0; c < _cols; ++c)
+				_matrix[r][c] = m._matrix[r][c];
+	}
+	return *this;
+}
+
 template<typename N>
 Matrix<N>& Matrix<N>::operator+=(const Matrix& m) {
-	if (m._rows != _rows && m._cols != _cols) 
+	// error if they don't have same shape
+	if (m._rows != _rows || m._cols != _cols) 
 		throw std::invalid_argument("arrays must have same size");
 
+	// add each element
 	for (int r = 0; r < _rows; ++r)
 		for (int c = 0; c < _cols; ++c)
 			_matrix[r][c] += m._matrix[r][c];
@@ -170,12 +340,25 @@ Matrix<N>& Matrix<N>::operator+=(const Matrix& m) {
 
 template<typename N>
 Matrix<N> operator+(Matrix<N> lhs, const Matrix<N>& rhs) {
+	// lhs is copy of argument, so we can use +=
 	lhs += rhs;
 	return lhs;
 }
 
 
+template<typename N>
+Matrix<N>& Matrix<N>::operator-=(const Matrix& m) {
+	// error if they don't have same shape
+	if (m._rows != _rows || m._cols != _cols)
+		throw std::invalid_argument("arrays must have same size");
+	
+	// subtract every element from *this
+	for (int r = 0; r < _rows; ++r) 
+		for (int c = 0; c < _cols; ++c)
+			_matrix[r][c] -= m._matrix[r][c];
 
+	return *this;
+}
 
 
 template<typename N>
@@ -186,6 +369,6 @@ Matrix<N>::~Matrix() {
 	delete[] _matrix;
 }
 
-}
+}	// math
 
 #endif
