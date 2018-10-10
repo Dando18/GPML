f0e5881e6717ee55ff16a5361a4fbd8834e62b6b
Daniel <dando18studios@gmail.com>
2018-10-10 17:17:01 -0400

    added doxygen support

--- include/typedefs.h
+++ include/typedefs.h
@@ -1,6 +1,16 @@
+/**	@file typedefs.h
+	Defines utilility types for math library.
+	@author Daniel Nichols
+	@date October 2018
+*/
+
 #ifndef _TYPEDEFS_H_
 #define _TYPEDEFS_H_
 
+namespace math {
+
+/** shorthand for unsigned int type */
 typedef unsigned int uint;
 
+}
 #endif
