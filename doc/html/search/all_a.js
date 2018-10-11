697d52e4fbe6c3345ecb4468e225f7408a0d8621
Daniel <dando18studios@gmail.com>
2018-10-10 23:22:50 -0400

    doxygen updates

--- doc/html/search/all_a.js
+++ doc/html/search/all_a.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['uint',['uint',['../typedefs_8h.html#a7b9b9413622e67b9df7f2d090b48682b',1,'math']]]
+  ['uint',['uint',['../typedefs_8h.html#a7b9b9413622e67b9df7f2d090b48682b',1,'math']]],
+  ['ul',['ul',['../typedefs_8h.html#ad71e7929690f0e5f868b5fb9b6ad36a8',1,'math']]],
+  ['ull',['ull',['../typedefs_8h.html#aa4f2818f8bf4329fd20d5517190a4d85',1,'math']]]
 ];
--- doc/html/search/functions_3.js
+++ doc/html/search/functions_3.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['operator_2a',['operator*',['../Matrix_8hpp.html#a8b2b9bc6e9c45e33a535f7b1650bc906',1,'math::operator*(Matrix&lt; N &gt; lhs, const N &amp;rhs)'],['../Matrix_8hpp.html#a990777f6ce4e2deb13a59f23622fd8bc',1,'math::operator*(const N &amp;lhs, Matrix&lt; N &gt; rhs)'],['../Matrix_8hpp.html#ab82237960c54e9b78d0721613a773ba9',1,'math::operator*(Matrix&lt; N &gt; lhs, const Matrix&lt; N &gt; &amp;rhs)']]],
+  ['operator_2a',['operator*',['../Matrix_8hpp.html#a8b2b9bc6e9c45e33a535f7b1650bc906',1,'math::operator*(Matrix&lt; N &gt; lhs, const N &amp;rhs)'],['../Matrix_8hpp.html#a990777f6ce4e2deb13a59f23622fd8bc',1,'math::operator*(const N &amp;lhs, Matrix&lt; N &gt; rhs)'],['../Matrix_8hpp.html#a6a581f190dd23d4400763318e86f9d53',1,'math::operator*(Matrix&lt; N &gt; lhs, const Matrix&lt; N &gt; &amp;rhs)']]],
   ['operator_2a_3d',['operator*=',['../classmath_1_1Matrix.html#a1704c2b9fd200a6854b6793524a15700',1,'math::Matrix::operator*=(const N &amp;scal)'],['../classmath_1_1Matrix.html#a4dbf1e0c5f42ce314ea2510a025b7e2a',1,'math::Matrix::operator*=(const Matrix &amp;m)']]],
   ['operator_2b',['operator+',['../Matrix_8hpp.html#a6e8f7c0c49b91a302a3c98c764d1bcf8',1,'math']]],
   ['operator_2b_3d',['operator+=',['../classmath_1_1Matrix.html#ae2944c1e1a0f1958101db2197bcfaa3c',1,'math::Matrix']]],
   ['operator_2d',['operator-',['../Matrix_8hpp.html#a9bd8120f9bdad37a57f74aa4bbfcde67',1,'math']]],
   ['operator_2d_3d',['operator-=',['../classmath_1_1Matrix.html#a046b9f4717da34fb08795cb461497084',1,'math::Matrix']]],
-  ['operator_2f',['operator/',['../Matrix_8hpp.html#a66c4aedee806ac605abc38c2b528e374',1,'math']]],
-  ['operator_2f_3d',['operator/=',['../classmath_1_1Matrix.html#a6a0a90575952ca84cac100cb40a5e9d0',1,'math::Matrix']]],
+  ['operator_2f',['operator/',['../Matrix_8hpp.html#a98e3ca1b2db938b945dc58ac8a02f49a',1,'math']]],
+  ['operator_2f_3d',['operator/=',['../classmath_1_1Matrix.html#a1145a22a8fa63ae3b8901184dd665e3a',1,'math::Matrix']]],
   ['operator_3d',['operator=',['../classmath_1_1Matrix.html#a8bf5d371b703ef3023742c4058f709a7',1,'math::Matrix']]]
 ];
